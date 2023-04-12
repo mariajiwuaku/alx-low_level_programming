2b85dedbb8e52f4baaeda94ce54a95af8b4bd2a2
Maria-goretti Jiwuaku <mariagorettiduruigbo@gmail.com>
2023-04-12 13:58:24 -0700

    task 2

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,10 +8,9 @@
  *
  * Return: Always 0
  */
-int main(int argc, char *argv[])
-{
-	(void) argc;
-	printf("%s\n", argv[0]);
+int main(int argc__attribute__((unused)), char *argv[])
+
+	printf("%s\n", *argv);
 
 	return (0);
 }
