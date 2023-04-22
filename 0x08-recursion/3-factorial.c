92c15f4be473cac8e7766a9fdae4a3c168f34818
Maria-goretti Jiwuaku <mariagorettiduruigbo@gmail.com>
2023-04-22 11:22:46 -0700

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,14 +4,14 @@ include "main.h"
  * factorial - Calculate the factorial of a number
  * @n: the number to calculate the factorial
  *
- * Return: integer value
+ * Return: factorial of n
  */
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
 
-	if (n <= 1)
+	if (n == 0)
 		return (1);
 
 	return (n * factorial(n - 1));
