8823161a436cd29c911ad453736e5108a72a3f27
Maria-goretti Jiwuaku <mariagorettiduruigbo@gmail.com>
2023-04-23 15:17:36 -0700

    static

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,20 @@
+/**
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: inputs value
+ * @n; input value
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	return (dest);
+}
